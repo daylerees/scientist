d2b508c7f302e1d363e78863b4fe6f8c56abf501
Will Rowe <wrowe@will-rowe.com>
2017-02-07 21:19:21 -0500

    Add a matcher that uses closures

---
+++ tests/Matchers/ClosureMatcherTest.php
@@ -0,0 +1,31 @@
+<?php
+
+use Scientist\Matchers\ClosureMatcher;
+
+class ClosureMatcherTest extends PHPUnit_Framework_TestCase
+{
+    private function getClosure()
+    {
+        return function ($control, $trial) {
+            return strtoupper($control) == strtoupper($trial);
+        };
+    }
+
+    public function test_that_closure_matcher_can_be_created()
+    {
+        $matcher = new ClosureMatcher($this->getClosure());
+        $this->assertInstanceOf(ClosureMatcher::class, $matcher);
+    }
+
+    public function test_that_closure_matcher_can_match_values()
+    {
+        $matcher = new ClosureMatcher($this->getClosure());
+        $this->assertTrue($matcher->match('uppercase', 'UpperCase'));
+    }
+
+    public function test_that_closure_matcher_can_fail_to_match_values()
+    {
+        $matcher = new ClosureMatcher($this->getClosure());
+        $this->assertFalse($matcher->match('uppercase', 'LowerCase'));
+    }
+}
